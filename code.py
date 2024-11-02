883c1f144f3afa104de4a67d3730a3658e935453
Tommy Thorn <tommy-github2@thorn.ws>
2024-11-02 01:06:52 -0700

    worked

--- code.py
+++ code.py
@@ -1,33 +1,20 @@
+from storage import getmount
+
+from kmk.kmk_keyboard import KMKKeyboard # as _KMKKeyboard
+from kmk.modules.split import Split, SplitSide
+from kmk.scanners import DiodeOrientation
+from kmk.keys import KC
 import board
 
-from kmk.kmk_keyboard import KMKKeyboard
-from kmk.keys import KC
-from kmk.scanners import DiodeOrientation
+# class KMKKeyboard(_KMKKeyboard):
 
 keyboard = KMKKeyboard()
-
-keyboard.col_pins = (
-board.GP28,
-board.GP27,
-board.GP26,
-board.GP22,
-board.GP20,
-board.GP23,
-board.GP21,
-)
+keyboard.col_pins = (board.GP28, board.GP27, board.GP26, board.GP22, board.GP20, board.GP23, board.GP21)
 keyboard.row_pins = (board.GP4,board.GP5,board.GP6,board.GP7,board.GP8,board.GP9)
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
-
-# keyboard.keymap = [
-#     [KC.A,KC.B,KC.C,KC.D,KC.E,KC.F,KC.G,
-#      KC.H,KC.I,KC.J,KC.K,KC.L,KC.M,KC.N,
-#      KC.O,KC.P,KC.Q,KC.R,KC.S,KC.T,KC.U,
-#      KC.V,KC.W,KC.X,KC.Y,KC.Z,KC.MINUS,KC.EQUAL,
-#      KC.N0,KC.N1,KC.N2,KC.N3,KC.N4,KC.N5,KC.N6,
-#      KC.N7,KC.N8,KC.N9,KC.LBRACKET,KC.RBRACKET,KC.BSLASH,
-#      ]
-# ]
-
+#split = Split()
+#split.data_pin = board.GP3
+#keyboard.modules.append(split)
 keyboard.keymap = [[
     KC.NO,		KC.N6,		KC.N7,		KC.N8,		KC.N9,		KC.N0,		KC.MINUS,
     KC.NO,		KC.F,		KC.G,		KC.C,		KC.R,		KC.L,		KC.SLASH,
@@ -38,6 +25,6 @@ keyboard.keymap = [[
 ]]
 
 if __name__ == '__main__':
-    print("Starting keyboard")
+    print("Keyboard ",getmount("/").label)
     keyboard.go()
     
